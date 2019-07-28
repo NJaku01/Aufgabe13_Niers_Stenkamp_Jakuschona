b39ae6af0496e14f4d992661745911f92486bfe9
nick1 <n_jaku01@wwu.de>
2019-07-28 13:32:12 +0200

    Server connecting to mongodb with docker

--- server.js
+++ server.js
@@ -3,24 +3,36 @@
 const express = require('express');
 const app = express();
 var server = require('http').createServer(app);
+
 const mongodb = require('mongodb');
 
+function connectMongoDb() {
+    // finish this block before the server starts,
+    // there are some async tasks inside we need to wait for => declare async so we can use await
+    (async () => {
 
-(async () => {
+        try {
+            // Use connect method to the mongo-client with the mongod-service
+            //                      and attach connection and db reference to the app
 
-    try {
-        // Use connect method to the mongo-client with the mongod-service
-        //                      and attach connection and db reference to the app
-        app.locals.dbConnection = await mongodb.MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
-        app.locals.db = await app.locals.dbConnection.db("itemdb");
-        console.log("Using db: " + app.locals.db.databaseName);
-    } catch (error) {
-        console.dir(error);
-    }
+            // using a local service on the same machine
+            //app.locals.dbConnection = await mongodb.MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
 
-    //mongo.close();
+            // using a named service (e.g. a docker container "mongodbservice")
+            app.locals.dbConnection = await mongodb.MongoClient.connect("mongodb://mongo:27017", {useNewUrlParser: true});
 
-})();
+            app.locals.db = await app.locals.dbConnection.db("itemdb");
+            console.log("Using db: " + app.locals.db.databaseName);
+        } catch (error) {
+            console.dir(error);
+            setTimeout(connectMongoDb, 3000); // retry until db-server is up
+        }
+
+    })();
+}
+
+connectMongoDb();
+
 
 
 // const dirname="C:/Users/nick1/OneDrive - uni-muenster.de/UNI/Geosoftware/Aufgabe_07_Jakuschona";
