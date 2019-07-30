e9e48f409d7b9434bd26e8be3b38833c46d5cd2f
nick1 <n_jaku01@wwu.de>
2019-07-30 19:06:50 +0200

    install turf finished

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.use(express.urlencoded({extended: false}));
 app.use("/leaflet", express.static(__dirname + "/node_modules/leaflet/dist"));
 app.use("/leaflet-control-geocoder", express.static(__dirname + "/node_modules/leaflet-control-geocoder/dist"));
 app.use("/leaflet-routing-machine", express.static(__dirname + "/node_modules/leaflet-routing-machine/dist"));
-app.use("@turf/line-intersect", express.static(__dirname + "node_modules/@turf/line-intersect"));
 app.use("/pub-pkg-bootstrap-min", express.static(__dirname + "/node_modules/pub-pkg-bootstrap-min"));
 app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use("/turf.js", express.static(__dirname + "/turf.js"))
