5e92d704d66889593e4d1d6b9f7cc034126dd250
NJaku01 <46575458+NJaku01@users.noreply.github.com>
2019-09-09 20:06:37 +0200

    Delete tokens.js

--- tokens.js
+++
@@ -1,8 +0,0 @@
-if (typeof exports == "undefined"){
-    var exports = window;
-};
-exports.token = {
-    MOVEBANK_login: "your-movebank-login",
-    MOVEBANK_password: "your-movebank-password",
-    OPENWEATHERMAP_TOKEN: 'your-openweathermap-token'
-};
