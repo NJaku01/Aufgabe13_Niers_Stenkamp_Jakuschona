1c37fe8b2192adbdba94bb6a116ad99a1c60d0af
NJaku01 <46575458+NJaku01@users.noreply.github.com>
2019-09-09 20:09:07 +0200

    Create tokens.js

---
+++ private/tokens.js
@@ -0,0 +1,8 @@
+ if (typeof exports == "undefined"){
+    var exports = window;
+ };
+ exports.token = {
+    MOVEBANK_login: "your-movebank-login",
+    MOVEBANK_password: "your-movebank-password",
+    OPENWEATHERMAP_TOKEN: 'your-openweathermap-token'
+ };
